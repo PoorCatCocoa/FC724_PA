8744183d349920faee17e171ce933b0131ab2818
PoorCatCocoa <104686890+PoorCatCocoa@users.noreply.github.com>
2025-01-18 23:59:46 +0000

    Ver.0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # FC724_PA
+# This is a music player based on Pygame and written by python
--- main.py
+++ main.py
@@ -1,3 +1,37 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
+from tkinter import messagebox
+import os
+import pygame
 
+class MusicPlayer:
+	def __init__(self):
+		self.window = tk.Tk()
+		self.window.title("Music Player")
+		self.window.geometry("750x400")
+
+		"""		设置背景图片
+		self.bg_image = tk.PhotoImage(file=os.path.join(os.getcwd(), "MusicPlayer/images", "bg_con.png"))
+		self.bg_label = ttk.Label(self.root, image=self.bg_image)
+		self.bg_label.place(relx=0, depend=- 0, relwidth=1, relheight=1) 
+		"""
+
+		win_style = ttk.Style()
+		win_style.theme_use('classic')
+		win_style.configure('.', background='black', foreground='black')
+		win_style.configure('TFrame', background='white', foreground='white')
+		win_style.configure('TButton', font=('Arial', 12), background='black', foreground='white',
+		activebackground='brown', activeforeground='white')
+		win_style.configure('TLabel', font=('Arial', 12), background='black', foreground='white')
+		win_style.configure('TScale', background='white')
+
+		pygame.init()
+		pygame.mixer.init()
+
+
+
+
+
+a = MusicPlayer()
+a.window.mainloop()
