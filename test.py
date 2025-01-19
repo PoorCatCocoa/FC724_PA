26026f61cd08be3254264f8f7365a8d7d1ed78af
PoorCatCocoa <104686890+PoorCatCocoa@users.noreply.github.com>
2025-01-19 21:53:12 +0000

    Ver.1.0.1

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+from logging import setLogRecordFactory
+from operator import length_hint
+
 import pygame
 import tkinter as tk
 from tkinter import ttk
@@ -15,7 +18,6 @@ class MusicPlayer:
         self.window.geometry("200x400")
 
         win_style = ttk.Style()
-        win_style.theme_use('clam')
 
         # 当前播放状态和音频文件
         self.current_file = None
@@ -45,11 +47,11 @@ class MusicPlayer:
         self.skip_back_button.grid(row=4, column=1, columnspan=1, pady=10, ipadx=12, ipady=2)
 
         # 显示时间标签
-        self.time_label = tk.Label(self.window, text="00:00 / 00:00", font=("Helvetica", 12))
+        self.time_label = ttk.Label(self.window, text="00:00 / 00:00", font=("Helvetica", 12))
         self.time_label.grid(row=5, column=0, columnspan=2, pady=5)
 
         # 进度条
-        self.progress = ttk.Scale(self.window, from_=0, to=100, orient="horizontal", command=self.update_progress)
+        self.progress = ttk.Scale(self.window, from_=0, to=200, orient="horizontal", command=self.update_progress, cursor="hand2")
         self.progress.grid(row=6, column=0, columnspan=2, padx=20, pady=10, sticky="ew")
 
     def load_music(self):
@@ -74,13 +76,13 @@ class MusicPlayer:
     def set_volume(self, volume):
         """设置音乐音量"""
         pygame.mixer.music.set_volume(float(volume))
-        print(f"Volume set to: {volume}")
 
     def skip_forward(self):
         """跳到音乐的下一个位置"""
         if self.current_file:
             current_pos = pygame.mixer.music.get_pos() / 1000.0
-            pygame.mixer.music.play(start=current_pos + 10)
+            pygame.mixer.music.play(start=current_pos + 10.0)
+            self.update_time_label(current_pos)
 
     def skip_back(self):
         """跳到音乐的上一个位置"""
@@ -88,6 +90,7 @@ class MusicPlayer:
             current_pos = pygame.mixer.music.get_pos() / 1000.0
             new_pos = max(0, int(current_pos) - 10)
             pygame.mixer.music.play(start=new_pos)
+            self.update_time_label(current_pos)
 
     def update_progress_bar(self):
         """更新进度条和时间显示"""
@@ -101,8 +104,9 @@ class MusicPlayer:
 
     def update_time_label(self, current_time):
         """更新时间标签"""
+        song_length = pygame.mixer.Sound(self.current_file).get_length()
         elapsed_minutes, elapsed_seconds = divmod(int(current_time), 60)
-        total_minutes, total_seconds = divmod(int(getattr(self, 'song_length', 0)), 60)
+        total_minutes, total_seconds = divmod(int(song_length), 60)
 
         self.time_label.config(
             text=f"{elapsed_minutes:02}:{elapsed_seconds:02} / {total_minutes:02}:{total_seconds:02}")
@@ -113,6 +117,5 @@ class MusicPlayer:
             pygame.mixer.music.rewind()
             pygame.mixer.music.set_pos(float(value))
             self.update_time_label(float(value))
-
 
 MusicPlayer().window.mainloop()
